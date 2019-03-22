aff251d2f473a373f38a34a0a0ca16b96ed66927
Андрей Астафьев <carrats@yandex.ru>
2019-03-22 22:20:15 +0300

    Добавил компонент поиска

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './page/main/main.component';
+import { SearchComponent } from './components/search/search.component';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, SearchComponent],
   imports: [
     CommonModule,
+    SharedModule,
     MainRoutingModule
   ]
 })
